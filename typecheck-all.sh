f7d9a04af76830c4f02c7b0b80e33f4f1016160e
Marco A. Feliu <marco.feliu@nianet.org>
2019-04-25 13:09:06 -0400

    Fix

--- typecheck-all.sh
+++ typecheck-all.sh
@@ -6,7 +6,7 @@ export PVS_DIR=/home/travis/build/test-mfeliu/pvslib/pvs
 echo PVS_DIR=$PVS_DIR
 export PATH=$PVS_DIR:$PATH
 echo PATH=$PATH
-cd $PVS_PATH/nasalib
+cd $PVS_DIR/nasalib
 pwd
-# ./typecheck-all
+./typecheck-all
 cat ./nasalib.summaries
