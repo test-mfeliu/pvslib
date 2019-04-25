ebd7dbdabed851ec47c37bb077ce7cf32d548155
Marco A. Feliu <marco.feliu@nianet.org>
2019-04-25 11:12:14 -0400

    Fix PVS/PVSLIB folder structure in install-dependencies.sh script

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -6,12 +6,14 @@ mkdir pvs
 cd pvs
 curl -o ./pvs.tgz "http://pvs.csl.sri.com/cgi-bin/download.cgi?file=pvs-6.0-ix86_64-Linux-allegro.tgz&accept=I+accept"
 tar xvfz ./pvs.tgz
+ls
 bin/relocate
 export PVS_PATH=`pwd`
 echo PVS_PATH=$PVS_PATH
-cd ..
 export PATH=$PVS_PATH:$PATH
 echo PATH=$PATH
+ln -s .. nasalib
+cd nasalib
 ./install-scripts
 ls
 
