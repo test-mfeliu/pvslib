f74f0cdb83e65ae21daffa8c4cc10289b3070a56
Marco A. Feliu <marco.feliu@nianet.org>
2019-04-25 11:07:23 -0400

    Fix pushd/popd in install-dependencies.sh script

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -3,13 +3,13 @@
 export PVS_LIBRARY_PATH=`pwd`
 echo PVS_LIBRARY_PATH=$PVS_LIBRARY_PATH
 mkdir pvs
-pushd pvs
+cd pvs
 curl -o ./pvs.tgz "http://pvs.csl.sri.com/cgi-bin/download.cgi?file=pvs-6.0-ix86_64-Linux-allegro.tgz&accept=I+accept"
 tar xvfz ./pvs.tgz
 bin/relocate
 export PVS_PATH=`pwd`
 echo PVS_PATH=$PVS_PATH
-popd
+cd ..
 export PATH=$PVS_PATH:$PATH
 echo PATH=$PATH
 ./install-scripts
