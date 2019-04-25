06c2505965317234b4864738d672d6575441019d
Marco A. Feliu <marco.feliu@nianet.org>
2019-04-25 11:03:05 -0400

    Install dependencies for travis-ci

---
+++ install-dependencies.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env sh
+
+export PVS_LIBRARY_PATH=`pwd`
+echo PVS_LIBRARY_PATH=$PVS_LIBRARY_PATH
+mkdir pvs
+pushd pvs
+curl -o ./pvs.tgz "http://pvs.csl.sri.com/cgi-bin/download.cgi?file=pvs-6.0-ix86_64-Linux-allegro.tgz&accept=I+accept"
+tar xvfz ./pvs.tgz
+bin/relocate
+export PVS_PATH=`pwd`
+echo PVS_PATH=$PVS_PATH
+popd
+export PATH=$PVS_PATH:$PATH
+echo PATH=$PATH
+./install-scripts
+ls
+
